cf0a8b1aaf8a2da43bcc5336aa90a0d15b2509ba
Ali Abdullah <ali.abdullah.skipq@gmail.com>
2023-02-25 21:26:28 +0500

    create lambda function

--- Ali_Abdullah/Training_Sprint1/training_sprint1/training_sprint1_stack.py
+++ Ali_Abdullah/Training_Sprint1/training_sprint1/training_sprint1_stack.py
@@ -1,4 +1,5 @@
 from aws_cdk import (
+    aws_lambda as lambda_,
     # Duration,
     Stack,
     # aws_sqs as sqs,
@@ -10,6 +11,14 @@ class TrainingSprint1Stack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        fn = self.create_lambda("HelloLambda","./resources","HelloWorldLambda.lambda_handler")
+
+    def create_lambda(self, id, asset, handler):
+        return lambda_.Function(self,
+        id = id,
+        handler = handler,
+        code=lambda_.Code.from_asset(asset),
+        runtime=lambda_.Runtime.PYTHON_3_9)
         # The code that defines your stack goes here
 
         # example resource
