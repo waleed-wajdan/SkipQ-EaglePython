75872a75e4fc68b5515b277d1b52d91d3c5b3615
waleed-wajdan <135476607+waleed-wajdan@users.noreply.github.com>
2023-06-04 05:05:58 +0500

    Delete mTanveer/Training_Sprint1 directory

--- mTanveer/Training_Sprint1/README.md
+++
@@ -1,5 +0,0 @@
-# Testing README FOR DEMO
-hello world
-
-## This is a Test
-hello world hello world
--- mTanveer/Training_Sprint1/bin/training_sprint1.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { TrainingSprint1Stack } from '../lib/training_sprint1-stack';
-
-const app = new cdk.App();
-new TrainingSprint1Stack(app, 'TrainingSprint1Stack');
